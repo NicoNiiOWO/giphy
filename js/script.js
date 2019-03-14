df2f0235f304fcc72cdcde6bdee342dfbb9c937b
Samuel Estevez <julioestevez@aclanyc.com>
2019-03-14 22:22:10 +0000

    yes

--- js/script.js
+++ js/script.js
@@ -4,8 +4,13 @@
 /* global $ */
 
 $("#search-button").click(function(){
-  
-  
-  
+    var search = "https://api.giphy.com/v1/gifs/search?q=" + $("input").val() + "&rating=pg&api_key=dc6zaTOxFJmzC";
+    $.ajax({
+        url: search,
+        method: "GET",
+        success: function(response) {
+            console.log(response.data[0].images.original.url);
+            $(".gallery").append("<img src='"+response.data[0].images.original.url+"'>");
+        }
+    }); 
 });
-
